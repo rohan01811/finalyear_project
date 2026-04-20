14b5b65e2a7eac5f6aa8ac2f0a2fd70debbe108e
webdev007 <22143002@pvgcoet.ac.in>
2026-04-20 16:45:48 +0530

    second commit

--- backend/config/supabase_client.py
+++ backend/config/supabase_client.py
@@ -2,9 +2,9 @@ from supabase import create_client, Client
 import os
 from supabase.lib.client_options import ClientOptions
 
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-SUPABASE_URL = "https://pouvubkqqagqprfkiwqs.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvdXZ1YmtxcWFncXByZmtpd3FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2MDc1NjgsImV4cCI6MjA4NzE4MzU2OH0.JKAqsMAqq4lZdpIfPXoLEf-CcMNJyPvuty0pDRuW3Zw"
 
 supabase: Client = create_client(
     SUPABASE_URL,
